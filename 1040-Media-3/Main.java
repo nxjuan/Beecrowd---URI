6a6ba942e61fd22a7004176eafa9016ea0793fd8
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-05-08 12:09:32 -0300

    ex 1014 corrigido

--- 1040-Media-3/Main.java
+++ 1040-Media-3/Main.java
@@ -1,33 +1,51 @@
+import java.io.IOException;
 import java.util.Scanner;
+import java.text.DecimalFormat;
+
 class Main {  
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    double n1, n2, n3, n4, newExam;
-    n1 = sc.nextDouble();
-    n2 = sc.nextDouble();
-    n3 = sc.nextDouble();
-    n4 = sc.nextDouble();
+    DecimalFormat df = new DecimalFormat("0.0");
+    df.setRoundingMode(java.math.RoundingMode.DOWN);
 
-    double m = ((n1*2) + (n2*3) + (n3 * 4) + n4) / 10;
+    String StringNotas = sc.nextLine();
+
+    String[] ArrayNota = StringNotas.split(" ");
+
+    double[] notas = new double[ArrayNota.length];
+
+    for (int i = 0; i < ArrayNota.length; i++){
+      notas[i] = Double.parseDouble(ArrayNota[i]);
+    }    
+
+    double m = ((notas[0]*2) + (notas[1]*3) + (notas[2] * 4) + notas[3]) / 10;
+
+    // System.out.println(StringNotas);
 
     if(m >= 7){
-      System.out.printf("Media: %.1f\n", m);
+      System.out.println("Media: " + df.format(m));
       System.out.println("Aluno aprovado.");
     }else if(m >= 5 && m < 7){
-      System.out.printf("Media: %.1f\n", m);
+      System.out.println("Media: " + df.format(m));
       System.out.println("Aluno em exame.");
-      System.out.print("Nota do exame: ");
-      newExam = sc.nextDouble(); 
-      m = (m + newExam) / 2;
+      
+      double notaExame = sc.nextDouble();
+      
+      System.out.println("Nota do exame: " + df.format(notaExame));
+      
+      // sc.nextLine(); // Consumir a nova linha pendente
+
+      m = (m + notaExame) / 2;
+
       if(m >= 5){
         System.out.println("Aluno aprovado.");
-        System.out.printf("Media final: %.1f\n", m);
+        System.out.println("Media final: " + df.format(m));
       }else{
         System.out.println("Aluno reprovado.");
-        System.out.printf("Media final: %.1f\n", m);
+        System.out.println("Media final: " + df.format(m));
       }
     }else{
-      System.out.printf("Media: %.1f\n");
+      System.out.println("Media: " + df.format(m));
       System.out.println("Aluno reprovado.");
     }
   }
