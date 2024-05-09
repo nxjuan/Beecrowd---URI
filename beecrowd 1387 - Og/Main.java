b8379183620cc51e0e8fc2c543b1ec6cd8c7d362
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-05-09 13:59:31 -0300

    1387 corrigido

--- beecrowd 1387 - Og/Main.java
+++ beecrowd 1387 - Og/Main.java
@@ -1,35 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        try{
-            String x = sc.nextLine();        
-            Integer n1 = (int)x.charAt(0) - 48;
-
-            if(x.length() > 1){
-                Integer n2 = (int)x.charAt(2) - 48;
-                if(n1 + n2 != 0){
-                    System.out.println(n1 + n2);
-                }else{
-                    System.out.println("");
-                }
-            }
-            else{
-                Integer n2 = 0;
-                if(n1 + n2 != 0){
-                    System.out.println(n1 + n2);
-                }else{
-                    System.out.println("");
-                }
-            }
-        }
-        catch(StringIndexOutOfBoundsException e){
-            System.out.print("");
-        }
-          
-        
-        sc.close();
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n1, n2;
+    
+    while(true){
+      n1 = sc.nextInt();
+      n2 = sc.nextInt();
+      if(n1 + n2 == 0){
+        break;
+      }
+      else{
+        System.out.println(n1 + n2);
+      }
     }
+  }
 }
