b78880b4c5743f0481f318b1c4e2a30d4f5c4850
Juan Nogueira <100308661+nxjuan@users.noreply.github.com>
2024-12-16 15:21:50 -0300

    new

--- Iniciante/1050 - DDD/Main.java
+++ Iniciante/1050 - DDD/Main.java
@@ -3,22 +3,22 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-
+ 
     public static void main(String[] args) throws IOException {
-
+ 
        HashMap<Integer, String> map = new HashMap<>();
        map.put(61, "Brasilia");
        map.put(71, "Salvador");
        map.put(11, "Sao Paulo");
        map.put(21, "Rio de Janeiro");
-       map.put(31, "Juiz de Fora");
-       map.put(69, "Campinas");
+       map.put(32, "Juiz de Fora");
+       map.put(19, "Campinas");
        map.put(27, "Vitoria");
        map.put(31, "Belo Horizonte");
        Scanner sc = new Scanner(System.in);
-       Integer val = sc.nextInt();
-       System.out.println(map.get(val));
-
+       int val = sc.nextInt();
+       System.out.println(map.containsKey(val) ? map.get(val) : "DDD nao cadastrado" );
+ 
     }
-
+ 
 }
